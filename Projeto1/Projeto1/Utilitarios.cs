3f56f0800b13c18d4eae37d55cdcdcff56413926
Daniel Henry <danielhenry1610@gmail.com>
2020-04-08 14:43:47 -0300

    selecionador de opções

---
+++ Projeto1/Projeto1/Utilitarios.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+class Utilitarios
+{
+    public static void EsperarEnter()
+    {
+        WriteLine("\n\n\nDigite [ENTER] para terminar a operação");
+        ReadLine();
+        Clear();
+    }
+    public static void EscreverXY(int col, int lin, string texto)
+    {
+        SetCursorPosition(col, lin);
+        Write(texto);
+    }
+}
+
+
